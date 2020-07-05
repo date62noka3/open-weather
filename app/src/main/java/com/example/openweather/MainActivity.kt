c0e2be6c7b3ecfc818abc10078f5f48877c7c394
t_kosuga <t.kosuga1741@morijyobi.ac.jp>
2020-07-05 18:42:01 +0900

    add library エリアデータ(Xml)を取得し、SimpleXmlでKotlinクラスに変換する

--- app/src/main/java/com/example/openweather/MainActivity.kt
+++ app/src/main/java/com/example/openweather/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.openweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.openweather.repository.WeatherRepos
 
 class MainActivity : AppCompatActivity() {
+    private val service = WeatherRepos().getRetrofit()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
